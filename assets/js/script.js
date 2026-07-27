e0823f7a513d6a25686fc81bcf3f49e52fae5418
Micah Francis <micah.francis69@googlemail.com>
2026-07-27 07:20:42 +0100

    styling the options and added the title to the array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,58 @@
-const questions = [
-  {
-    question: "What is the capital of Jamaica?",
-    options: ["Kingson", "St. James", "Montego Bay", "St. Elizabeth"],
-    answer: 0
-  },
-  {
-    question: "What is the capital of Italy?",
-    options: ["Naples", "Turin", "Milan", "Rome"],
-    answer: 3
-  },
-  {
-    question: "What is the capital of Japan?",
-    options: ["Okinawa", "Tokyo", "Fukushima", "Hokkaido"],
-    answer: 1
-  },
-  {
-    question: "What is the capital of Brazil?",
-    options: ["Salvador", "Sao Paulo", "Rio de Janeiro", "Brasilia"],
-    answer: 3
-  },
-  {
-    question: "What is the capital of Ghana?",
-    options: ["Sekondi-Takoradi", "Accra", "Kumasi", "Tamale"],
-    answer: 1
-  },
-  {
-    question: "What is the capital of Portugal?",
-    options: ["Lisbon", "Porto", "Braga", "Coimbra"],
-    answer: 0
-  },
-  {
-    question: "What is the capital of Mali?",
-    options: ["Sikasso", "Segou", "Timbuktu", "Bamako"],
-    answer: 3
-  },
-  {
-    question: "What is the capital of Guyana?",
-    options: ["Linden", "Georgetown", "New Amsterdam", "Rose Hall"],
-    answer: 1
-  },
-  {
-    question: "What is the capital of Canada?",
-    options: ["Ottawa", "Alberta", "Quebec", "Ontario"],
-    answer: 0
-  },
-  {
-    question: "What is the capital of India?",
-    options: ["Chennai", "New Delhi", "Mumbai", "Hyderabad"],
-    answer: 1
-  },
-]
+const geography = {
+    title: "Geography",
+    questions: [
+        {
+            question: "What is the capital of Jamaica?",
+            options: ["Kingson", "St. James", "Montego Bay", "St. Elizabeth"],
+            answer: 0
+        },
+        {
+            question: "What is the capital of Italy?",
+            options: ["Naples", "Turin", "Milan", "Rome"],
+            answer: 3
+        },
+        {
+            question: "What is the capital of Japan?",
+            options: ["Okinawa", "Tokyo", "Fukushima", "Hokkaido"],
+            answer: 1
+        },
+        {
+            question: "What is the capital of Brazil?",
+            options: ["Salvador", "Sao Paulo", "Rio de Janeiro", "Brasilia"],
+            answer: 3
+        },
+        {
+            question: "What is the capital of Ghana?",
+            options: ["Sekondi-Takoradi", "Accra", "Kumasi", "Tamale"],
+            answer: 1
+        },
+        {
+            question: "What is the capital of Portugal?",
+            options: ["Lisbon", "Porto", "Braga", "Coimbra"],
+            answer: 0
+        },
+        {
+            question: "What is the capital of Mali?",
+            options: ["Sikasso", "Segou", "Timbuktu", "Bamako"],
+            answer: 3
+        },
+        {
+            question: "What is the capital of Guyana?",
+            options: ["Linden", "Georgetown", "New Amsterdam", "Rose Hall"],
+            answer: 1
+        },
+        {
+            question: "What is the capital of Canada?",
+            options: ["Ottawa", "Alberta", "Quebec", "Ontario"],
+            answer: 0
+        },
+        {
+            question: "What is the capital of India?",
+            options: ["Chennai", "New Delhi", "Mumbai", "Hyderabad"],
+            answer: 1
+        },
+    ]
+}
 
 let shuffled = [];
 let index = 0
@@ -58,21 +61,24 @@ let userAnswers = []
 let currentData = []
 let gameEnd = false
 
-// const startBtn = document.getElementById("startBtn");
 const prv = document.getElementById("prv");
 const nxt = document.getElementById("nxt");
-const h2 = document.querySelector("h2");
+const h1 = document.querySelector("h1");
+const h3 = document.querySelector("h3");
 const ol = document.querySelector("ol");
 const span = document.createElement("span");
 span.textContent = score
 
-const startQuiz = (data) => {
-    currentData = data
+const startQuiz = (quiz) => {
+    h1.textContent = quiz.title
+    currentData = quiz.questions
     document.getElementById("overlay").classList.add("remove")
-    shuffled = [...data].sort(() => Math.random() - 0.5).slice(0, 5);
+    shuffled = [...quiz.questions].sort(() => Math.random() - 0.5).slice(0, 5);
+    // Shuffles the questions array reducing them to 5.
     update()
 }
 
+// Let the user replay the same subject instead of going back to home
 const replay = () => {
     index = 0;
     userAnswers = [];
@@ -85,18 +91,20 @@ const replay = () => {
 }
 
 const update = () => {
-  h2.textContent = `${index + 1}. ${shuffled[index].question}`;
+  h3.textContent = `${index + 1}. ${shuffled[index].question}`;
   ol.innerHTML = ""
 
   nxt.disabled = true;
   shuffled[index].options.forEach((opt, i) => {
     const li = document.createElement("li");
+    li.classList.add("options")
     const input = document.createElement("input");
     const label = document.createElement("label");
     input.type = "radio"
     input.name = "answer"
     input.value = i
 
+    // listens for when an option has been check and disables the next button
     input.addEventListener("change", () => {
       const checked = document.querySelector("input[type='radio']:checked")
       nxt.disabled = !checked
